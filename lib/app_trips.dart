782b32e989e66f7aa28eba138d2c441e4810822d
Miguel Arenas <Miguel.Arenas@geniussports.com>
2021-11-14 02:28:27 -0500

    Create navigation bar

---
+++ lib/app_trips.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'home_trips.dart';
+import 'search_trips.dart';
+import 'profile_trips.dart';
+
+class AppTrips extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AppTrips();
+  }
+  
+}
+
+class _AppTrips extends State<AppTrips> {
+  int indexTap = 0;
+
+  final List<Widget> widgetsChildren = [
+    HomeTrips(),
+    SearchTrips(),
+    ProfileTrips()
+  ];
+
+  void onTapTapped(int index) {
+    setState(() {
+      indexTap = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widgetsChildren[indexTap],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.white,
+          primaryColor: Colors.purple
+        ),
+        child: BottomNavigationBar(
+          currentIndex: indexTap,
+          onTap: onTapTapped,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+              label: "",
+              icon: Icon(Icons.home)
+            ),
+            BottomNavigationBarItem(
+              label: "",
+              icon: Icon(Icons.search)
+            ),
+            BottomNavigationBarItem(
+              label: "",
+              icon: Icon(Icons.person)
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
---
+++ lib/home_trips.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'description_place.dart';
+import 'header_appbar.dart';
+import 'review_list.dart';
+
+class HomeTrips extends StatelessWidget {
+  final String descriptionDummy = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat blandit lectus ac sollicitudin. Etiam nisl turpis, aliquet a ipsum vitae, rutrum posuere eros. Quisque sed ornare nibh. Fusce scelerisque augue at velit luctus imperdiet. Quisque at mi placerat, gravida augue in, accumsan lorem. Suspendisse feugiat mattis justo nec viverra. Vivamus quis augue eu eros placerat pulvinar eu ac justo.
+
+  Nunc lectus mauris, egestas porta dui nec, sollicitudin ultricies eros. Integer ut pellentesque sem. Nam mollis tincidunt efficitur. Praesent interdum lacus efficitur ex aliquet molestie. Proin in bibendum tortor, quis volutpat orci. Etiam lectus diam, placerat non leo ac, faucibus aliquam ligula. Proin eu iaculis tellus. Pellentesque nec ultricies arcu, eu sagittis justo.
+
+  Sed odio dolor, volutpat euismod nisl nec, interdum pellentesque tortor. Nulla vel ullamcorper erat. Nunc scelerisque metus sagittis elementum convallis. Sed vitae mi eu tellus euismod malesuada ac ut ligula. Quisque at lectus mollis, interdum quam nec, convallis turpis. Suspendisse potenti. Ut vitae maximus odio, eu accumsan est. Mauris sed ullamcorper urna, ut egestas ex. Maecenas ut viverra nisl. Duis pulvinar facilisis ante. Mauris ut semper massa. Donec vitae purus elit. Vivamus elementum mi eget venenatis elementum. Nullam quis venenatis diam.
+
+  Nullam sodales quam justo, sit amet condimentum ante ultricies a. Donec justo elit, luctus et auctor eget, pellentesque id odio. Etiam enim risus, molestie a nisl volutpat, ultricies laoreet felis. Nulla fermentum odio at nulla cursus tincidunt. Sed vel orci ligula. Integer eget nisl augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus a hendrerit ipsum. Sed in metus aliquam, facilisis mauris eu, mollis metus. Sed ut auctor augue, sagittis sollicitudin enim. Integer est elit, vehicula eu aliquet eget, viverra a tortor. Vestibulum ultrices fringilla ipsum sit amet pretium. Proin mollis, est id suscipit viverra, tortor ipsum fermentum magna, venenatis vulputate diam dolor semper risus.''';
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ListView(
+          children: [
+            DescriptionPlace("Duwili Ella", 5, descriptionDummy),
+            ReviewList()
+          ],
+        ),
+        HeaderAppBar()
+      ],
+    );
+  }
+
+}
