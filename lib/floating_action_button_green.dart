4fc02c0b136549faef3a38eeceeb65dd89a92ae2
Miguel Arenas <Miguel.Arenas@geniussports.com>
2021-11-14 02:10:43 -0500

    Create custom floating action button

---
+++ lib/floating_action_button_green.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class FloatingActionButtonGreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FloatingActionButtonGreen();
+  }
+}
+
+class _FloatingActionButtonGreen extends State<FloatingActionButtonGreen> {
+
+  void onPressedFav() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          content: Text("Favorite")
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      backgroundColor: Color(0xFF11DA53),
+      mini: true,
+      tooltip: "Fav",
+      onPressed: onPressedFav,
+      child: Icon(
+        Icons.favorite_border
+      ),
+    );
+  }
+
+}
