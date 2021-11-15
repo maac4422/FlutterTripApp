0a03fe0658222802044e609bf16adf5b70896b7d
Miguel Arenas <Miguel.Arenas@geniussports.com>
2021-11-14 22:41:09 -0500

    Set android statusBar transparent

--- android/app/src/main/kotlin/com/example/flutter_trip/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_trip/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.flutter_trip
-
+import android.annotation.TargetApi
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+    }
 }
