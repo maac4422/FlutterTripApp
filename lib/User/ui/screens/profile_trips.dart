45ed74b4f7e98526cc7bed3e725ebea518c9efdb
Miguel Arenas <Miguel.Arenas@geniussports.com>
2021-11-14 19:05:03 -0500

    Create user widgets

--- lib/User/ui/screens/profile_trips.dart
+++ lib/User/ui/screens/profile_trips.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'profile_header.dart';
+import '../widgets/profile_places_list.dart';
+import '../widgets/profile_background.dart';
 
 class ProfileTrips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueAccent,
+    return Stack(
+      children: <Widget>[
+        ProfileBackground(),
+        ListView(
+          children: <Widget>[
+            ProfileHeader(),
+            ProfilePlacesList()
+          ],
+        ),
+      ],
     );
   }
 
